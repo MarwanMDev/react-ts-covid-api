e7189743399323e52e41095cb8e53e2a850e5714
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-05 20:06:40 +0200

    integrating api

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const options = {
+    method: 'GET',
+    url: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
+    headers: {
+      'X-RapidAPI-Key':
+        '43ba294a17mshda1b9904dc000ffp19d49fjsn36d66232a6a9',
+      'X-RapidAPI-Host':
+        'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+    },
+  };
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        console.table(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
